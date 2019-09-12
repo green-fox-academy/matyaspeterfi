b84acccaaf2a603d73cd88ad540bc7daf788cdc6
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-12 14:11:24 +0200

    Added solution for hello-others task

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log ("Hello, Krisz")
+console.log ("Hello, Orsi!")
+console.log ("Hello, Pali!")
