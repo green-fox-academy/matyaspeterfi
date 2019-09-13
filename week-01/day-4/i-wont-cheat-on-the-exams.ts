3885ea118aa35773b1307bb130b4b3416e779e7a
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-13 11:04:57 +0200

    added solution for i-wont-cheat-on-the-exams task

---
+++ week-01/day-4/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let loopNumber: number = 0
+let honestMe: string = "I won't cheat on the exam!"
+
+while (loopNumber < 101) {
+    
+    console.log(honestMe);
+    loopNumber++
+}
+
+for (let loopCount:number = 0; loopCount<101; loopCount++) {
+    console.log(honestMe);
+}
