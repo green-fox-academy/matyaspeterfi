296c82d7b857efa1cd951b6b43380c1d2241a14b
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-15 19:06:42 +0200

    added solution for third task

---
+++ week-02/day-1/third.ts
@@ -0,0 +1,10 @@
+// -  Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers`
+
+
+let magicNumbers: number[] = [1, 3, 4, 7];
+
+magicNumbers[2] = 5
+
+console.log(magicNumbers[2])
