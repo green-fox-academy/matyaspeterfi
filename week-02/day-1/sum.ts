27cccda17037a7689b98de9ec18ce21cf4d18878
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-16 14:18:31 +0200

    added solution for sum task

---
+++ week-02/day-1/sum.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
+
+function sum(a){
+
+    a = ((a * (a+1))/2)
+
+    console.log(a)
+
+}
+
+
+sum (10)
