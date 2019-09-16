3cac1f8c90e0044c3e106002e7fefeafdc036043
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-16 20:21:20 +0200

    added solution for sum-all task

---
+++ week-02/day-1/sum-all.ts
@@ -0,0 +1,9 @@
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
+
+
+let ai:number[] = [3, 4, 5, 6, 7];
+
+let sum = ai.reduce((a, b) => a + b, 0);
+
+console.log(sum)
