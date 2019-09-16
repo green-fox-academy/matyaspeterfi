01dd34671f93bd8d832fc9995d0a6559f7d373dc
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-16 11:30:44 +0200

    added solution for swap-elements task

---
+++ week-02/day-1/swap-elements.ts
@@ -0,0 +1,16 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+
+let abc:string[] = ["Arthur", "Boe", "Chloe"]
+
+let [first, second, third] = abc
+
+abc = [first, second, third] = [third, second, first]
+
+
+
+
+
+
+console.log(abc)
