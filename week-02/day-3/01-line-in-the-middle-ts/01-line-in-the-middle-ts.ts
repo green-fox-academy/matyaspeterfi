d8d4a073ffba024e67d213f4a4d18771528f0f0f
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-18 12:11:42 +0200

    added solution for line in the middle task

---
+++ week-02/day-3/01-line-in-the-middle-ts/01-line-in-the-middle-ts.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
+
+
+ctx.beginPath()
+ctx.moveTo(0, (canvas.height / 2));
+ctx.lineTo(canvas.width, (canvas.height / 2));
+ctx.strokeStyle = 'red';
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(canvas.width / 2, 0);
+ctx.lineTo(canvas.width / 2, canvas.height);
+ctx.strokeStyle = "green";
+ctx.stroke();
