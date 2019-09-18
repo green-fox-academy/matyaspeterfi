258537c5525e87b7199a7ad0ccb80e34bf3ed671
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-18 13:47:52 +0200

    added solution for to the center task

---
+++ week-02/day-3/04-to-the-center/04-to-the-center-ts.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws a single line and takes 2 parameters:
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas
+// Draw at least 3 lines with that function using a loop.
+
+
+function zoltan(x, y):number{
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(canvas.width/2, canvas.height/2);
+    ctx.stroke();
+
+    return
+};
+
+zoltan(11, 21);
+
