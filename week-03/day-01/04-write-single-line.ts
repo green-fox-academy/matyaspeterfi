20eaa3c8eed6a6cec6091d27ff0b98a7362696f3
Mat Peterfi <matyas.peterfi@gmail.com>
2019-09-24 20:05:57 +0200

    added write single line solution and txt files that go with various solutions

---
+++ week-03/day-01/04-write-single-line.ts
@@ -0,0 +1,22 @@
+'use strict'
+
+// Write a function that is able to manipulate a file
+// By writing your name into it as a single line
+// The file should be named "my-file.txt"
+// In case the program is unable to write the file,
+// It should print the following error message: "Unable to write file: my-file.txt"
+
+
+const fs = require('fs');
+
+
+function writeMyName(input: string){
+    try{
+    fs.writeFileSync('my-file.txt', input , 'utf8');
+    }
+    catch{
+        console.log("Unable to write file: my-file.txt")
+    };
+};
+
+writeMyName("How do I break you?");
