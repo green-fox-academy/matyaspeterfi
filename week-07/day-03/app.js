a6934b62561d28b19e8515da9a4c04e3cd50d90a
Mat Peterfi <matyas.peterfi@gmail.com>
2019-10-30 14:31:09 +0100

    added solution for greeter exercise

--- week-07/day-03/app.js
+++ week-07/day-03/app.js
@@ -12,9 +12,10 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+//***********DOUBLING****************/
 app.get('/doubling', (req, res) => {
 
-  if (req.query.input !== undefined) {
+  if (req.query.input) {
     let input = req.query.input;
     let result = input * 2;
 
@@ -23,7 +24,31 @@ app.get('/doubling', (req, res) => {
       'result': result
     }
     res.send(output);
-  }else{
+  } else {
     res.send('Please provide an input!')
   }
 })
+
+//***********GREETER****************/
+
+app.get('/greeter', (req, res) => {
+  let name = req.query.name;
+  let title = req.query.title;
+  if (name == undefined && title == undefined) {
+    res.send({
+      'error': 'Please provide a name and a title!'
+    });
+  }else if(title == undefined){
+    res.send({
+      'error' : 'Please provide a title!'
+    })
+  } else if(name == undefined){
+    res.send({
+      'error': 'Pleae provide a name!'
+    })
+  } else {
+    res.send({
+      'welcome_message': `Oh, hi there ${name}, my dear ${title}!`
+    })
+  }
+});
