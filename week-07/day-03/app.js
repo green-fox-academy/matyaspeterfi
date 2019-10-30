96f70151726eb62100696b9f9bad902041c67b1d
Mat Peterfi <matyas.peterfi@gmail.com>
2019-10-30 15:36:25 +0100

    re-factored previous exercises and finished append-a exercise

--- week-07/day-03/app.js
+++ week-07/day-03/app.js
@@ -14,41 +14,85 @@ app.get('/', function (req, res) {
 
 //***********DOUBLING****************/
 app.get('/doubling', (req, res) => {
-
+let output;
   if (req.query.input) {
     let input = req.query.input;
     let result = input * 2;
 
-    let output = {
+    output = {
       'received': input,
       'result': result
     }
-    res.send(output);
   } else {
-    res.send('Please provide an input!')
+    output = 'Please provide an input!'
   }
+  res.send(output)
 })
 
 //***********GREETER****************/
 
 app.get('/greeter', (req, res) => {
-  let name = req.query.name;
+
+  let name = req.query['name'];
   let title = req.query.title;
-  if (name == undefined && title == undefined) {
-    res.send({
-      'error': 'Please provide a name and a title!'
-    });
-  }else if(title == undefined){
-    res.send({
-      'error' : 'Please provide a title!'
-    })
-  } else if(name == undefined){
-    res.send({
-      'error': 'Pleae provide a name!'
-    })
+  let paramsToCheck = [
+    'name',
+    'title'
+  ];
+
+  let missingParameters = [];
+
+  paramsToCheck.forEach((element) => {
+    if (req.query[element] == undefined) {
+        missingParameters.push(element); 
+    }
+  });
+
+  if (missingParameters.length > 0) {
+    result = {
+      'error': `Please provide a ${missingParameters.join(' and a ')}!`
+    };
   } else {
-    res.send({
+    result = {
       'welcome_message': `Oh, hi there ${name}, my dear ${title}!`
-    })
+    }
   }
+  res.send(result)
+
+  
+  // if (name == undefined && title == undefined) {
+  //   res.send({
+  //     'error': 'Please provide a name and a title!'
+  //   });
+  // } else if (title == undefined) {
+  //   res.send({
+  //     'error': 'Please provide a title!'
+  //   })
+  // } else if (name == undefined) {
+  //   res.send({
+  //     'error': 'Pleae provide a name!'
+  //   })
+  // } else {
+  //   res.send({
+  //     'welcome_message': `Oh, hi there ${name}, my dear ${title}!`
+  //   })
+  // } res.send(response)
 });
+
+//***********APPEND A****************//
+
+app.get('/appenda', (req, res)=>{
+  res.status(404)
+  res.send({
+    'response': '404: Not Found'
+  });
+})
+app.get('/appenda/:appendable', (req, res) => {
+  let input = req.params.appendable;
+  res.send({
+    'appended': `${input}a`
+  })
+})
+
+//***********DO UNTIL****************//
+
