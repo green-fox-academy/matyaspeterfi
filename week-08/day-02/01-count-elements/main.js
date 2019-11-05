98f507c865c53360acfc451c235f326ea707f86d
Mat Peterfi <matyas.peterfi@gmail.com>
2019-11-05 11:44:41 +0100

    added folder structure for week-08, and also solution for the first exercise of day-02

---
+++ week-08/day-02/01-count-elements/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let btn = document.querySelector('button');
+let listItems = document.querySelectorAll('li');
+let result = document.querySelector('.result');
+
+btn.addEventListener('click', () =>{
+  result.innerHTML =`The result is:  ${listItems.length}`;
+})
+
+
+
